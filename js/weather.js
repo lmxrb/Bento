3dd102def7c833773efc75fc36fc5cbeb3602785
lmxrb <lolmegaxd1@gmail.com>
2021-03-11 19:00:12 +0000

    Update weather

--- js/weather.js
+++ js/weather.js
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 41.126128;
+  let longitude = -8.606007;
 
   getWeather(latitude, longitude);
 }
